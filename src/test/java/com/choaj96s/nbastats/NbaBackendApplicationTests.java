547aed88c035925a2e29dd5cf1c90b636a52188b
Allen Cho <choaj96s@gmail.com>
2026-01-18 21:53:37 -0800

    initial setup

---
+++ src/test/java/com/choaj96s/nbastats/NbaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choaj96s.nbastats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
